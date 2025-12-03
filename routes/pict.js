b71aafab0ea90aa871a1a834fa8834d5c2392fdd
JuliaKozhevnikova <Kozhevnikova1501@yandex.ru>
2025-12-03 11:41:32 +0300

    test

--- routes/pict.js
+++ routes/pict.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 const { getAllPict } = require('../repository');
 
-
-router.get('/',  async function(req, res) {
+router.get('/', async function (req, res) {
   try {
-const data  = await getAllPict()
-res.send(data);
-  } catch(err) {
-console.log(err)
-  }  
+    const data = await getAllPict();
+    res.send(data);
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
